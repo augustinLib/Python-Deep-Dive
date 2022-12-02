19447b47cf7f355ba480fc584c045460310ff644
augustinLib <2bambitious@gmail.com>
2022-12-03 02:37:02 +0900

    Docs: Add studied contents
    about decorator

--- decorator_closure/decorator_basic.py
+++ decorator_closure/decorator_basic.py
@@ -1,16 +1,17 @@
-def deco(func):
-    def inner():
-        print("running inner()")
-    # deco()가 inner()함수 객체를 반환한다.
-    return inner
+def logger(func):
+    def wrapper(arg):
+        print("function start")
+        func(arg)
+        print("function end")
 
-@deco
-# target()을 deco로 decorate!
-def target():
-    print("running target()")
+    return wrapper
 
 
-# decorated된 target()을 호출하면 실제로는 inner()을 실행함
-target()
-# target이 inner()를 가리키고 있다.
-print(target)
+@logger
+def say_hello(name):
+    print("Hello", name)
+
+
+@logger
+def say_bye(name):
+    print("Bye", name)
